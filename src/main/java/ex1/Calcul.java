6230517e450da30b3c2a60ff2bed26c7341ad69a
Arnaud-C18 <clavier.a18@gmail.com>
2024-10-29 13:41:14 +0100

    Exercice 1 terminé

---
+++ src/main/java/ex1/Calcul.java
@@ -0,0 +1,29 @@
+package ex1;
+
+/**
+ * La classe Calcul permet d'effectuer des opérations de base
+ * comme l'addition et la soustraction sur des entiers.
+ */
+
+public class Calcul {
+
+	/**
+	 * Additionne deux entiers.
+	 * @param a premier entier
+	 * @param b deuxième entier
+	 * @return la somme de a et b
+	 */
+	public int addition(int a, int b){
+		return a+b;
+	}
+
+	/**
+	 * Soustrait deux entiers.
+	 * @param a premier entier
+	 * @param b deuxième entier
+	 * @return la différence entre a et b
+	 */
+	public int soustraction(int a, int b){
+		return a-b;
+	}
+}
---
+++ src/main/java/ex1/Entreprise.java
@@ -0,0 +1,28 @@
+package ex1;
+
+import java.util.Date;
+
+/** Représente une entreprise */
+public class Entreprise {
+
+	/** Numero de SIRET de l'entreprise */
+	public int Siret;
+
+	/** Nom de l'entreprise */
+	public String Nom;
+
+	/** Adresse de l'entreprise */
+	public String adresse;
+
+	/** Date de création de l'entreprise */
+	public Date dateCreation;
+
+	/** Capital maximum de l'entreprise */
+	public static final int CAPITAL_MAX = 3000000;
+
+	/** Affichage du statut de l'entreprise */
+	public void afficherStatut(){
+		
+	}
+	
+}
